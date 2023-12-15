d56cd467d5fc50360633bf1b1ec8c48467ebe265
Mercy <mercy.mwongeli@student.moringaschool.com>
2023-12-15 17:21:23 +0300

    done the project

--- lib/seed.py
+++ lib/seed.py
@@ -5,7 +5,7 @@ import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import Cleaner, CleaningTask, Client, ClientTask
+from models import Cleaner, CleaningTask, Client, ClientTask, Base
 
 fake = Faker()
 experience_levels = ["Junior", "Intermediate", "Senior"]
@@ -54,6 +54,7 @@ cleaning_tasks = [
 if __name__ == '__main__':
 
     engine = create_engine('sqlite:///clean_slate.db')
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     # empty previous data
